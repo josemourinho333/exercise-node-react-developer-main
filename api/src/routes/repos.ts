620ca6d277d0dc68e1c49bc81e9b3b066e1648fd
root <root@PhilYoo.localdomain>
2022-09-21 19:28:08 -0700

    Add initial fetch logs

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,4 +1,6 @@
 import { Router, Request, Response } from 'express';
+import localData from '../../data/repos.json';
+import axios from 'axios';
 
 export const repos = Router();
 
@@ -7,6 +9,21 @@ repos.get('/', async (_: Request, res: Response) => {
 
   res.status(200);
 
+  // console.log('hello', localData);
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
+  axios
+    .get('https://api.github.com/users/silverorange/repos/44')
+    .then((response) => {
+      response.data;
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log('Error Code:', err.response.status);
+      // eslint-disable-next-line no-console
+      console.log(
+        'Error Message:',
+        `Path (${err.response.request.path}) ${err.response.statusText}`
+      );
+    });
   res.json([]);
 });
